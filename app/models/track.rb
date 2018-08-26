593010dd905c826693b4cf323e35548a148559ab
Slawomir Wdowka <swdowka@gmail.com>
2018-08-26 19:39:01 +0200

    Push track to party’s playlist when new created.

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,4 +3,10 @@ class Track < ApplicationRecord
   belongs_to :guest
 
   validates_presence_of :name, :address, :party, :guest
+
+  after_create :notify_party
+
+  def notify_party
+    Pusher.trigger("party-#{party_id}", 'new-track', self.as_json)
+  end
 end
